e1ef7f510eba084f409c3fd44723bab712c30541
unknown <mirzetsikira@hotmail.com>
2024-04-30 10:30:16 +0200

    t

--- src/App.js
+++ src/App.js
@@ -34,12 +34,12 @@ function App() {
         <Slider {...settings}>
           <div className="slide-container">
             <img src={nanocgm} alt='' />
-            <div className="text-overlay">Sensor S9 CGM</div>
+            <div className="text-overlay">S9 CGM System</div>
             <div className="text-overlay2">Sistem za kontinuirano <br></br>praćenje glikemije</div>
           </div>
           <div className="slide-container">
             <img src={touchcare} alt='' />
-            <div className="text-overlay">Touchcare S9 CGM</div>
+            <div className="text-overlay">S9 CGM System</div>
             <div className="text-overlay2">Sistem za kontinuirano <br></br>praćenje glikemije</div>
           </div>
           <div className="slide-container">
